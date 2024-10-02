55e7ced5f8be4080de9ff0ebe9b51153aeb6712c
Chevyself <chevyself@gmail.com>
2024-10-01 19:49:28 -0500

    build fixes

--- svelte.config.js
+++ svelte.config.js
@@ -3,20 +3,19 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
+			fallback: null,
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: false
-		})
+			preprocess: false,
+			strict: true
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
